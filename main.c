80f8ad70f969567be0b1412c120f5fb54c167252
Julio <132522218+juliodf24@users.noreply.github.com>
2025-10-09 14:03:13 +0000

    adicionando struct

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
-int main(){
-    printf("boa noite !\n");
+#include "tipos.c"
+
+
+int main() {
+    struct tipoPergunta pergunta;
+
+    pergunta.nivel = 1;
+    strcpy(pergunta.enunciado, "Qual é o país conhecido como o berço do futebol? 💂🏻");
+    strcpy(pergunta.alternativas[0], "Brasil");
+    strcpy(pergunta.alternativas[1], "Inglaterra");
+    strcpy(pergunta.alternativas[2], "Alemanha");
+    strcpy(pergunta.alternativas[3], "Espanha");
+    pergunta.respostaCorreta = 1;
+    strcpy(pergunta.dica, "O esporte surgiu no século XIX em uma ilha europeia.");
+
+    printf("Nível %d\n", pergunta.nivel);
+    printf("%s\n", pergunta.enunciado);
+    for (int i = 0; i < 4; i++) {
+        printf("%d) %s\n", i + 1, pergunta.alternativas[i]);
+    }
+    printf("Dica: %s\n", pergunta.dica);
+    printf("Resposta correta: %s\n", pergunta.alternativas[pergunta.respostaCorreta]);
+
     return 0;
 }
---
+++ tipos.c
@@ -0,0 +1,7 @@
+struct tipoPergunta {
+    int nivel;
+    char enunciado[250];
+    char alternativas[4][250];
+    int respostaCorreta;
+    char dica[250];
+};
