da336dd949e64ebdd23104a2a29cd3cfc2c3463c
Júlio César <132522218+juliodf24@users.noreply.github.com>
2025-11-18 23:21:17 +0000

    atualizacao

---
+++ build/_deps/nixjson-build/CMakeFiles/nixjson_example1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nixjson_example1.dir/examples/example1.c.o"
+  "CMakeFiles/nixjson_example1.dir/examples/example1.c.o.d"
+  "nixjson_example1"
+  "nixjson_example1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nixjson_example1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
